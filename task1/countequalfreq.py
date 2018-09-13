c214d5133c123847fbc1ce11df158a725bc764c7
Lohitaksh yogi <lyogi@uncc.edu>
2018-09-13 18:18:48 -0400

    Delete countequalfreq.py

--- task1/countequalfreq.py
+++
@@ -1,9 +0,0 @@
-Given array of integers, check whether each integer, that occurs in it, is contained there the same number of times as any other integer from the given array.
-
-Example
-
-For inputArray = [1, 2, 2, 1], the output should be
-checkEqualFrequency(inputArray) = true;
-For inputArray = [1, 2, 2, 3, 1, 3, 1, 3], the output should be
-checkEqualFrequency(inputArray) = false.
-
