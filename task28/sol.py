85a61fa31f7b571bdfb0f4f3eae3ac62ff60448b
Lohitaksh yogi <lyogi@uncc.edu>
2018-09-27 20:24:12 -0400

    Update sol.py

--- task28/sol.py
+++ task28/sol.py
@@ -1,3 +1,4 @@
+#A = [1,2,3,-4,3,1,1,1]
 def func1(sum_check):
     max_sum = max(sum_check)
     compare = []
@@ -8,38 +9,39 @@ def func1(sum_check):
             compare.append(i)
     return compare
 
- sum1 = 0
- big_array = []
- index = []
- len1 = 0
- if len(A)==0:
-        return []
-        
- for i in range(len(A)):
-     if A[i] > 0 and len(A)-1 != i:
-         index.append(A[i])
-     else:
-         if i == len(A)-1 and A[i]>0:
-             index.append(A[i])
-         big_array.append(index)
-         index = []
+sum1 = 0
+big_array = []
+index = []
+len1 = 0
+if len(A)==0:
+    print("empty")
+else:
+    for i in range(len(A)):
+        if A[i] > 0 and len(A)-1 != i:
+            index.append(A[i])
+        else:
+            if i == len(A)-1 and A[i]>0:
+                index.append(A[i])
+            big_array.append(index)
+            index = []
 #print(big_array)    
- sum_check = list(map(sum, big_array))
- compare = func1(sum_check)
- if len(compare) == 1:
-     print(big_array[compare[0]])
- else:
-     x = len(compare)
-     i = 0
-     newlist = []
-     while x > 0:
-         newlist.append([len(big_array[compare[i]]),compare[i]])
-         i+=1
-         x -= 1
-     compare = func1([row[0] for row in newlist])
-     if len(compare) == 1:
-         print(big_array[newlist[compare[0]][1]])
-     else:
-         minindex = min([row[1] for row in newlist])
-         print(big_array[minindex])
-        
+    sum_check = list(map(sum, big_array))
+    #print(sum_check,big)
+    
+    compare = func1(sum_check)
+    if len(compare) == 1:
+        print(big_array[compare[0]])
+    else:
+        x = len(compare)
+        i = 0
+        newlist = []
+        while x > 0:
+            newlist.append([len(big_array[compare[i]]),compare[i]])
+            i+=1
+            x -= 1
+        compare = func1([row[0] for row in newlist])
+        if len(compare) == 1:
+            print(big_array[newlist[compare[0]][1]])
+        else:
+            minindex = min([row[1] for row in newlist])
+            print(big_array[minindex])
