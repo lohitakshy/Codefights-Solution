fa572bf2a704f03aaedd1cc6419df829cb276170
Lohitaksh yogi <lohitakshyogi@gmail.com>
2018-10-02 15:17:03 -0400

    Update avoidObstacles.py

--- avoidObstacles/avoidObstacles.py
+++ avoidObstacles/avoidObstacles.py
@@ -3,7 +3,7 @@
 def avoidObstacles(inputArray):
     check_hit = 1
     while True:
-        if sorted([value%check_hit for value in inputArray])[0]>0:
+        if sorted([value%check_hit for value in inputArray])[0]>0: # sorted [0] for minimum value of the jump
             return check_hit
         check_hit += 1
             
