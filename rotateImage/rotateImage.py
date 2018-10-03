51a8239787d74722c2c709a7645107daa14ed61d
Lohitaksh yogi <lohitakshyogi@gmail.com>
2018-10-03 16:46:56 -0400

    Update rotateImage.py

--- rotateImage/rotateImage.py
+++ rotateImage/rotateImage.py
@@ -1,2 +1,10 @@
 def rotateImage(a):
     return numpy.rot90(a,k=3)
+-------------------------------------------------
+# other solution
+def rotateImage(a):
+    a.reverse()
+    for i in range(len(a)):
+        for j in range(i):
+            a[i][j], a[j][i] = a[j][i], a[i][j]
+    return a
