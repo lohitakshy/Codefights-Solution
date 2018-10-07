3d694bf6caa0b84adfdfe93e9cd32b11be413427
Lohitaksh yogi <lohitakshyogi@gmail.com>
2018-10-07 14:25:44 -0400

    Update variableName.py

--- variableName/variableName.py
+++ variableName/variableName.py
@@ -7,3 +7,10 @@ def variableName(name):
         else:
             return False
     return True
+
+------------------------------
+# Compare like this 
+ if (not ('a' <= name[i] and name[i] <= 'z' or
+                    'A' <= name[i] and name[i] <= 'Z' or
+                    '0' <= name[i] and name[i] <= '9' or
+                    name[i] == '_')):
